1be18533449e42ac3b56a61bdd317d875b67d014
emmanuelorji2013 <emmanuelorji2013@gmail.com>
2019-08-29 20:54:12 +0100

    Fixed clear screen bug.

--- calc/script.js
+++ calc/script.js
@@ -4,10 +4,7 @@ var displayValue = "0";
 var waitingForOperator = false;
 var isNewOperation = false;
 var canPoint = true;
-<<<<<<< HEAD
-var isAnswer = false
-=======
->>>>>>> 3a5766e89ee18939694e8fb9a28d8666b2c44428
+//var isAnswer = false;
 
 
 updateDisplayValue = (e) => {
@@ -17,9 +14,9 @@ updateDisplayValue = (e) => {
     case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "0": {
         
         //if(isNewOperation)
-        if(displayValue === "0" || isNewOperation) {
+        if((displayValue === "0") || isNewOperation) {
             displayValue = "";
-            display(displayValue);
+            //display(displayValue);
             isNewOperation = false;
         }
         displayValue += buText;
@@ -31,7 +28,7 @@ updateDisplayValue = (e) => {
     //Addition, multiplication and Division Signs
     case "+": case "-": case "x": case "/": case "^": {
         buText = (buText == "x")? "*": buText;  //Replaces x with *
-        if((waitingForOperator && displayValue !== "0") ) {
+        if(waitingForOperator && (displayValue !== "0") ) {
             
             displayValue = (isNewOperation && !waitingForOperator )? "": displayValue;
             displayValue += buText
@@ -71,6 +68,7 @@ updateDisplayValue = (e) => {
         break;
     }
     case "(-)": {
+        
         displayValue = (displayValue[0] == "-")? displayValue.slice(2,displayValue.length - 1): "-(" + displayValue + ")";
         display(displayValue);
         break;
@@ -98,6 +96,9 @@ updateDisplayValue = (e) => {
 function clearScreen() {
     displayValue = "0";
     display(displayValue);
+    waitingForOperator = true;
+    isNewOperation = false;//Allow the zero to be used when a point is the next input
+    canPoint = true;
 }
 
 function display(displayVal) {
@@ -106,7 +107,7 @@ function display(displayVal) {
 
 function performOperation(displayVal) {
     //displayValue = displayVal
-    if(displayValue && displayValue !== "0") {
+    if(displayVal && (displayVal !== "0")) {
         try {
             var convertedDisplayValue = displayVal.toString().replace(/\u005E/g, "**"); // (/\u005E/g) is regular expression for all appearances of (^)
             displayValue = eval(convertedDisplayValue).toString();
@@ -114,7 +115,7 @@ function performOperation(displayVal) {
             canPoint = displayValue.includes(".")? false: canPoint; //Disable addition of point if answer contains "." 
             
             display(displayValue);
-            isAnswer = true;
+            //isAnswer = true;
         } catch (error) {
             alert(error);
         }
@@ -128,3 +129,10 @@ function performOperation(displayVal) {
 for (let i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener("click", updateDisplayValue);
 }
+
+/*
+TODO:   (1) Button (-) shouldn't work when display is zero or empty
+        (2) Use regular expression to make sure (-) works well when minus sign is to be removed from a number that 
+            is not in the form -(xxx)
+        (3) Make expressions of the form x(y) work by putting * whenever a number is just before "(" or just after ")"
+*/
